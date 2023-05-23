029dc14e1e314c9ff1c627286975a125290fcda7
inNetto <inetto.ferreira@gmail.com>
2023-05-23 20:55:28 -0300

    Issue #2

---
+++ resources/js/Pages/Item/Partials/MyFooterDialogModal.vue
@@ -0,0 +1,10 @@
+<script setup>
+import JetSecondaryButton from "@/Components/SecondaryButton.vue";
+
+const emit = defineEmits(["closeModal"]);
+</script>
+<template>
+    <jet-secondary-button @click="$emit('closeModal', false)">
+        Exit
+    </jet-secondary-button>
+</template>
---
+++ resources/js/Shared/MyDialogModal.vue
@@ -0,0 +1,23 @@
+<script setup>
+import JetDialogModal from "@/Components/DialogModal.vue";
+import MyFooterDialogModal from "../Pages/Item/Partials/MyFooterDialogModal.vue";
+
+const props = defineProps({
+    opened: { type: Boolean, default: false },
+    title: { type: String, default: "Title here" },
+    content: { type: Array, default: undefined },
+});
+</script>
+<template>
+    <JetDialogModal :show="opened" @close="opened = false">
+        <template #title> {{ props.title }} </template>
+
+        <template #content>
+            {{ props.content }}
+        </template>
+
+        <template #footer>
+            <MyFooterDialogModal @closeModal="(value) => (opened = value)" />
+        </template>
+    </JetDialogModal>
+</template>
