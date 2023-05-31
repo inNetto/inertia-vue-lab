972fe507d2e6bbb92de72f42aef0001c91931ce5
inNetto <inetto.ferreira@gmail.com>
2023-05-31 20:15:13 -0300

    issue #5

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -1,16 +1,16 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from "vue";
 
 defineProps({
-    modelValue: String,
+    modelValue: [String, Number],
 });
 
-defineEmits(['update:modelValue']);
+defineEmits(["update:modelValue"]);
 
 const input = ref(null);
 
 onMounted(() => {
-    if (input.value.hasAttribute('autofocus')) {
+    if (input.value.hasAttribute("autofocus")) {
         input.value.focus();
     }
 });
@@ -24,5 +24,5 @@ defineExpose({ focus: () => input.value.focus() });
         class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
-    >
+    />
 </template>
